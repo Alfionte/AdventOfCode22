8432654fdd80b7fc8df5355946b77eab55aa79ab
alfionte <gabriele.porcelli@gmail.com>
2022-12-04 12:09:37 +0000

    Day 4 - Partial overlapping assignments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,15 +8,20 @@ fun main() {
     // Day 1
     GetMaxCaloriesUseCase().getMaxCalories().also { println("max calories: $it") }
     GetMaxCaloriesUseCase().getTop3Count().also { println("Top 3 calories count: $it") }
+    println()
 
     // Day 2
     RockPaperScissorsUseCase().getPointsForTheGuide().also { println("Points from following the guide: $it") }
     RockPaperScissorsUseCase().getPointsForTheGuideV2().also { println("Points from following the guide V2: $it") }
+    println()
 
     // Day 3
     RuckSacksUseCase().getRuckSackPriorities().also { println("RuckSack priorities sum: $it") }
     RuckSacksUseCase().getBadgePriorities().also { println("Badge priorities sum: $it") }
+    println()
 
     // Day 4
-    CleaningAreasUseCase().getOverlapAssignment().also { println("Overlapping assignments: $it") }
+    CleaningAreasUseCase().getTotalOverlapAssignments().also { println("Total overlapping assignments: $it") }
+    CleaningAreasUseCase().getPartialOverlapAssignments().also { println("Partial overlapping assignments: $it") }
+    println()
 }
--- src/main/kotlin/day4/CleaningAreasUseCase.kt
+++ src/main/kotlin/day4/CleaningAreasUseCase.kt
@@ -7,7 +7,7 @@ class CleaningAreasUseCase {
     private val input = File("src/main/resources/day4/input.txt")
     private val regex = "(\\d+)-(\\d+),(\\d+)-(\\d+)".toRegex()
 
-    fun getOverlapAssignment(): Int =
+    fun getTotalOverlapAssignments(): Int =
         input
             .readLines()
             .mapNotNull {
@@ -18,8 +18,22 @@ class CleaningAreasUseCase {
             }
             .count()
 
+    fun getPartialOverlapAssignments(): Int =
+        input
+            .readLines()
+            .mapNotNull {
+                val (min1, max1, min2, max2) = getRanges(it)
+                val range1 = (min1.toInt()..max1.toInt())
+                val range2 = (min2.toInt()..max2.toInt())
+                if (range1 isOverlapping range2 || range2 isOverlapping range1) Unit else null
+            }
+            .count()
+
     private fun getRanges(line: String) = regex.find(line)!!.destructured
 
     private infix fun IntRange.isIncludedIn(range: IntRange) = this.first >= range.first && this.last <= range.last
+    private infix fun IntRange.isOverlapping(range: IntRange) =
+        (this.first >= range.first && this.first <= range.last) ||
+                (this.last >= range.first && this.last <= range.last)
 
 }
